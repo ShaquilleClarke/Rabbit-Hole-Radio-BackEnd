6afe57eb1b73d31aacd9f9cd05e1a715df975d28
ShaquilleClarke <github email address>
2020-04-03 01:02:08 -0400

    got some auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,42 @@
 class UsersController < ApplicationController
+    before_action :authorized, only: [:persist]
+
+    def index
+        user = User.all 
+        render json: user
+    end
+
+    def create
+        @user = User.create(user_params)
+        if @user.valid? 
+          wristband = encode_token({user_id: @user.id})
+          render json: { user: UserSerializer.new(@user), token: wristband }, status: 201
+        else
+        render json: {error: "Oh no you diiiddn't"}  
+        end
+    end
+
+    def login
+        @user = User.find_by(username: params[:username])
+        
+        if @user && @user.authenticate(params[:password_digest])
+          wristband = encode_token({user_id: @user.id})
+          render json: { user: UserSerializer.new(@user), token: wristband }
+        else 
+          render json: {error: "Thats not you!"} 
+        end
+    end
+
+    def persist
+        wristband = encode_token({user_id: @user.id})
+        render json: { user: UserSerializer.new(@user), token: wristband }
+    end 
+
+    private
+
+    def user_params
+        params.permit(:username, :password_digest)
+    end
+    
+
 end
